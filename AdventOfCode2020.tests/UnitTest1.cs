6dcb96a44f8a6dbc1b16c5d053a969365f1b4014
Kaspars Kalnins <joxxxter@gmail.com>
2020-12-02 19:48:51 +0200

    added test project

---
+++ AdventOfCode2020.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2020.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
